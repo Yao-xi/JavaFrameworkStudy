46a9fe891ebdd9309e368af4c48a22f74ec57f07
Yao-xi <664898320@qq.com>
2020-12-19 16:57:30 +0800

    默认更改列表

--- ExerciseDay01_spring2/src/test/java/com/yx/service/UserServiceTest.java
+++ ExerciseDay01_spring2/src/test/java/com/yx/service/UserServiceTest.java
@@ -4,8 +4,6 @@ import com.yx.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,7 +21,7 @@ public class UserServiceTest {
         System.out.println(list);
     }
     
-    public void test02(){
+    public void test02() {
     
     }
 }
