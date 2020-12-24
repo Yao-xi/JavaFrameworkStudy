98fa467025c24555467b243eceb26b5b03083825
Yao-xi <664898320@qq.com>
2020-12-24 21:01:41 +0800

    默认更改列表

--- day05_springmvc/src/main/java/com/yx/controller/ResponseController.java
+++ day05_springmvc/src/main/java/com/yx/controller/ResponseController.java
@@ -1,7 +1,15 @@
 package com.yx.controller;
 
+import com.yx.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/resp")
@@ -13,4 +21,36 @@ public class ResponseController {
         // 不写默认转发
         return "redirect:/success.jsp";
     }
+    
+    @RequestMapping("/show2")
+    @ResponseBody
+    public User show2() {
+        return new User("张三", 23);
+    }
+    
+    @RequestMapping("/show3")
+    @ResponseBody
+    public List<User> show3() {
+        ArrayList<User> list = new ArrayList<>();
+        Collections.addAll(list,
+                           new User("张三", 23),
+                           new User("李四", 24),
+                           new User("王五", 25));
+        return list;
+    }
+    
+    @RequestMapping("/show4")
+    @ResponseBody
+    public List<User> show4(@RequestBody List<User> userList) {
+        System.out.println(userList);
+        return userList;
+    }
+    
+    @RequestMapping("/show5")
+    @ResponseBody
+    public List<String> show5(@RequestParam("userList") List<String> userList) {
+        System.out.println(userList);
+        return userList;
+    }
+    
 }
--- day05_springmvc/src/main/webapp/index.jsp
+++ day05_springmvc/src/main/webapp/index.jsp
@@ -2,8 +2,31 @@
 <html>
 <head>
     <title>Title</title>
+    <script src="${pageContext.request.contextPath}/js/jquery-3.3.1.min.js"></script>
 </head>
 <body>
-    <a href="${pageContext.request.contextPath}/save">save</a>
+    <a href="${pageContext.request.contextPath}/user/save">save</a>
+
+    <button id="btn">点我</button>
+    <button id="btn2">点我</button>
+
+    <textarea id="txt"></textarea>
 </body>
 </html>
+<script>
+    $("#btn1").click(function() {
+        let arr = [{username: 'zs', age: 16}, {username: 'ls', age: 17}, {username: '王五', age: 18}];
+        console.log(JSON.stringify(arr));
+        $.ajax({
+            type: "POST",
+            url: "${pageContext.request.contextPath}/resp/show4",
+            data: JSON.stringify(arr),
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function(data) {
+                console.log(data);
+                alert(data);
+            }
+        });
+    });
+</script>
