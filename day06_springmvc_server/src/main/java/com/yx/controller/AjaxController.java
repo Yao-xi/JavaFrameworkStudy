f07e1541272b5b8731b40f7f4a1ebce2855398fd
Yao-xi <664898320@qq.com>
2020-12-25 20:57:10 +0800

    默认更改列表

--- day06_springmvc_server/src/main/java/com/yx/controller/AjaxController.java
+++ day06_springmvc_server/src/main/java/com/yx/controller/AjaxController.java
@@ -1,6 +1,8 @@
 package com.yx.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,11 +13,27 @@ import java.io.IOException;
 
 @Controller
 @RequestMapping("/ajax")
+@CrossOrigin // 允许跨域
 public class AjaxController {
     @RequestMapping("/show1")
     @ResponseBody
     public String show1() {
         return "success";
+    }
+    
+    @RequestMapping("/show2")
+    @ResponseBody
+    public String show2(String name, Integer age) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        return name + " " + age;
+    }
+    
+    @RequestMapping("/show3")
+    @ResponseBody
+    public String show3(@RequestBody String str) {
+        System.out.println(str);
+        return str;
     }
     
     @RequestMapping("/upload")
