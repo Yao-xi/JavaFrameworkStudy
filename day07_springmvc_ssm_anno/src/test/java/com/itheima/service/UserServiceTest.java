8760bb4452d9c9f394d2c92e591a2f4953b4c84a
Yao-xi <664898320@qq.com>
2020-12-28 19:12:30 +0800

    默认更改列表

--- day07_springmvc_ssm_anno/src/test/java/com/itheima/service/UserServiceTest.java
+++ day07_springmvc_ssm_anno/src/test/java/com/itheima/service/UserServiceTest.java
@@ -8,10 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
 @ContextConfiguration(classes = {SpringConfig.class})
 public class UserServiceTest {
     
