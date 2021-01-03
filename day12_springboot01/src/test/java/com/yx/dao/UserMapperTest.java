767ad52b5c43fc69002eca7661484abc5bb6d29c
Yao-xi <664898320@qq.com>
2021-01-03 17:46:28 +0800

    默认更改列表

--- day12_springboot01/src/test/java/com/yx/dao/UserMapperTest.java
+++ day12_springboot01/src/test/java/com/yx/dao/UserMapperTest.java
@@ -4,10 +4,12 @@ import com.yx.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.List;
 
 @SpringBootTest
+@EnableTransactionManagement
 class UserMapperTest {
     
     @Autowired
