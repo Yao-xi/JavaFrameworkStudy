0fd1ad46bd20469804a4f6c791105dda2049e5c2
Yao-xi <664898320@qq.com>
2021-01-11 11:51:59 +0800

    默认更改列表

--- day15-elasticsearch/src/test/java/com/yx/dao/UserPlusMapperTest.java
+++ day15-elasticsearch/src/test/java/com/yx/dao/UserPlusMapperTest.java
@@ -1,13 +1,16 @@
 package com.yx.dao;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yx.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 @EnableTransactionManagement
@@ -17,12 +20,36 @@ public class UserPlusMapperTest {
     
     @Test
     void testFindAll() {
-        QueryWrapper<User> wrapper = new QueryWrapper<>();
-        List<User> list = userMapper.selectList(wrapper);
+        List<User> list = userMapper.selectList(null);
         System.out.println(list);
     }
     
     @Test
-    void test01(){
+    void test01() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id", 6);
+        User user = userMapper.selectOne(queryWrapper);
+        System.out.println(user);
+    }
+    
+    @Test
+    void test02() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("gender", "count(id)").groupBy("gender");
+        List<Map<String, Object>> list = userMapper.selectMaps(queryWrapper);
+        System.out.println(list);
+    }
+    
+    @Test
+    void test03() {
+        int i = userMapper.insert(new User(null, "aaaaab", "aaa",
+                                           "aaa", 0, new Date()));
+        System.out.println("已更新: " + i);
+    }
+    
+    @Test
+    void test04() {
+        Page<User> userPage = userMapper.selectPage(new Page<>(1, 2), null);
+        System.out.println(userPage);
     }
 }
